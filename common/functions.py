076ebbdc6b0fcd482f353538de14b16d0bab4709
Xiaodong Li <bryne_lxd@sina.com>
2019-11-12 21:08:43 +0800

    two layer net and relative train demo added

--- common/functions.py
+++ common/functions.py
@@ -1,7 +1,10 @@
 import numpy as np
 
 def sigmoid(x):
-    return 1 / (1 + np.exp(-x))    
+    return 1 / (1 + np.exp(-x))   
+
+def sigmoid_grad(x):
+    return (1.0 - sigmoid(x)) * sigmoid(x) 
 
 def softmax(x):
     if x.ndim == 2:
